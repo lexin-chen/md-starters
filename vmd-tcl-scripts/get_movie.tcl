4e3320cc53859df7d8dd7ac7b7429b40faf88856
Lexin Chen <110304515+lexinsea@users.noreply.github.com>
2022-10-20 22:21:24 -0400

    Update get_movie.tcl

--- vmd-tcl-scripts/get_movie.tcl
+++ vmd-tcl-scripts/get_movie.tcl
@@ -8,5 +8,5 @@ set n [molinfo top get numframes]
 for { set i 0 } { $i < $n } { incr i } {
 	animate goto $i
 	display update
-	render TachyonInternal "C:/Users/lexin/Downloads/new3/$i.png" -fullshade -auto_skylight 0.7 -aasamples 12 %s -format PNG -res 6000 4000 -o %s.png
+	render TachyonInternal "C:/Users/lexin/Downloads/$i.png" -fullshade -auto_skylight 0.7 -aasamples 12 %s -format PNG -res 6000 4000 -o %s.png
 }
